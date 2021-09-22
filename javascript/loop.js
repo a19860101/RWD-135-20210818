a80ff128fd2a36f1e8261451c0938499456c3fca
Zac <a19860101@gmail.com>
2021-09-22 21:57:05 +0800

    20210922

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,5 +1,14 @@
 // for
 
 for(let i=0;i<5;i++){
-    console.log(i);
+    // console.log(i);
+    document.write(i);
+}
+
+// while
+
+let j = 0;
+while(j < 10){
+    j += 2;
+    console.log(j);
 }
