945a53b5deac90ef5acbeb3eef7f17f721ecbb50
Zac <a19860101@gmail.com>
2021-09-22 21:18:37 +0800

    20210922

---
+++ javascript/condition.js
@@ -0,0 +1,34 @@
+//if
+let x =1;
+
+if(x > 0){
+    console.log('a: success');
+}
+
+//if...else
+
+if(x > 0){
+    console.log('b: success');
+}else{
+    console.log('b: error');
+}
+
+// if...else if
+if(x > 10){
+    console.log('A');
+}else if(x > 6){
+    console.log('B')
+}else if(x > 2){
+    console.log('C');
+}
+
+//if...else if...else
+if(x > 10){
+    console.log('A');
+}else if(x > 6){
+    console.log('B')
+}else if(x > 2){
+    console.log('C');
+}else{
+    console.log('不及格');
+}
--- javascript/operator.js
+++ javascript/operator.js
@@ -43,3 +43,8 @@ console.log(a > 0 || b < 0);
 
 let i = 12;
 console.log(!i);
+
+// 三元運算子
+
+let result = a < 0 ? '成立':'不成立';
+console.log(result);
