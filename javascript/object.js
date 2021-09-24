61c64e576d7d7b42165026a74208524d52e3fcc4
Zac <a19860101@gmail.com>
2021-09-24 21:24:39 +0800

    20210924

---
+++ javascript/object.js
@@ -0,0 +1,15 @@
+// const user = new Object();
+// user.name = 'John';
+// user.age = 20;
+// user.mail = 'john@gmail.com';
+
+const user = {
+    name: 'John',
+    age: 20,
+    mail: 'john@gmail.com'
+}
+
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+console.log(user.mail);
