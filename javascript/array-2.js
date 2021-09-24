3e81306589cb67208b51425ff46d48f1ce93969c
Zac <a19860101@gmail.com>
2021-09-24 20:20:36 +0800

    20210924

--- javascript/array-2.js
+++ javascript/array-2.js
@@ -4,3 +4,14 @@ const drinks = ['紅茶','綠茶','奶茶','拿鐵'];
 drinks.forEach(function(data,idx){
     console.log(data,idx);
 })
+
+//map
+const scores = [56,72,82,40,90,68];
+const scores_map = scores.map(function(data){
+    if(data >= 60){
+        return '及格';
+    }else{
+        return '不及格';
+    }
+})
+console.log(scores_map);
