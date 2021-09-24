a2e740c4a0a7b897f5051879c0f0dcfca0275330
Zac <a19860101@gmail.com>
2021-09-24 19:31:20 +0800

    20210924

--- javascript/array.js
+++ javascript/array.js
@@ -5,7 +5,7 @@
 
 // let a = new Array('Apple','Banana','Cat');
 
-let a = ['Apple','Banana','Cat'];
+let a = ['Apple','Banana','Cat','Dog','egg'];
 
 
 // console.log(a);
@@ -14,16 +14,29 @@ let a = ['Apple','Banana','Cat'];
 // console.log(a[2]);
 
 //陣列迭代
-for(let i=0;i<3;i++){
-    console.log(a[i])
+// console.log(a.length);
+
+for(let i=0;i<a.length;i++){
+    console.log('for',a[i])
 }
 
 // for...of
 for(data of a){
-    console.log(data);
+    console.log('of',data);
 }
 
 // for...in
 for(data in a){
-    console.log(a[data]);
+    console.log('in',a[data]);
 }
+
+//let vs const
+
+let x = [1,2,3,4];
+x = ['a','b','c'];
+console.log(x);
+
+const y = [1,2,3,4];
+y.pop();
+y.push(5);
+console.log(y);
