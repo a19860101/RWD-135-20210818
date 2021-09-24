0102f38e97e228f88fdbb038f44c4a01135ec101
Zac <a19860101@gmail.com>
2021-09-24 19:21:16 +0800

    20210924

--- javascript/array.js
+++ javascript/array.js
@@ -8,4 +8,22 @@
 let a = ['Apple','Banana','Cat'];
 
 
-console.log(a);
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+
+//陣列迭代
+for(let i=0;i<3;i++){
+    console.log(a[i])
+}
+
+// for...of
+for(data of a){
+    console.log(data);
+}
+
+// for...in
+for(data in a){
+    console.log(a[data]);
+}
