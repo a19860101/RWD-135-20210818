ad801021fa52d5b77e7e663f7f5c0e539546dbd2
Zac <a19860101@gmail.com>
2021-09-24 21:32:42 +0800

    20210924

--- javascript/object.js
+++ javascript/object.js
@@ -13,3 +13,25 @@ console.log(user);
 console.log(user.name);
 console.log(user.age);
 console.log(user.mail);
+
+const users = [
+    {
+        id: 1,
+        name: 'john',
+        mail: 'john@gmail.com'
+    },{
+        id:2,
+        name:'Mary',
+        mail:'maryyyy@gmail.com'
+    },{
+        id:3,
+        name:'Max',
+        mail:'xxx@gmail.com'
+    }
+];
+console.log(users);
+users.forEach(function(user){
+    console.log(user.id);
+    console.log(user.name);
+    console.log(user.mail);
+})
