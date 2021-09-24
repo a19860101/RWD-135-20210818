0da4c29d38b06d22387bc23c6ec5391d260bfb24
Zac <a19860101@gmail.com>
2021-09-24 19:40:45 +0800

    20210924

--- javascript/array.js
+++ javascript/array.js
@@ -16,27 +16,39 @@ let a = ['Apple','Banana','Cat','Dog','egg'];
 //陣列迭代
 // console.log(a.length);
 
-for(let i=0;i<a.length;i++){
-    console.log('for',a[i])
-}
+// for(let i=0;i<a.length;i++){
+//     console.log('for',a[i])
+// }
 
 // for...of
-for(data of a){
-    console.log('of',data);
-}
+// for(data of a){
+//     console.log('of',data);
+// }
 
 // for...in
-for(data in a){
-    console.log('in',a[data]);
-}
+// for(data in a){
+//     console.log('in',a[data]);
+// }
 
 //let vs const
 
-let x = [1,2,3,4];
-x = ['a','b','c'];
-console.log(x);
+// let x = [1,2,3,4];
+// x = ['a','b','c'];
+// console.log(x);
 
-const y = [1,2,3,4];
-y.pop();
-y.push(5);
-console.log(y);
+// const y = [1,2,3,4];
+// y.pop();
+// y.push(5);
+// console.log(y);
+
+//length
+// console.log(a.length);
+
+//push(),pop()
+a.pop();
+a.push('hello');
+
+//unshift(),shift()
+a.unshift('test');
+a.shift();
+console.log(a);
