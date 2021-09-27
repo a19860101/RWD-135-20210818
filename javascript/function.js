ff66ee576f7aba01e050161d5b404fa01734c901
Zac <a19860101@gmail.com>
2021-09-27 19:07:26 +0800

    20210927

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+function f1(){
+    let str = 'this is funcion one';
+    return str;
+}
+console.log(f1());
+
+const f2 = function(){
+    let str = 'this is function two';
+    return str;
+}
+
+console.log(f2());
+
