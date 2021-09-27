033066ba95b600618bc886476fa85da9bc90e7fe
Zac <a19860101@gmail.com>
2021-09-27 19:19:27 +0800

    20210927

--- javascript/function.js
+++ javascript/function.js
@@ -1,13 +1,32 @@
+
+//函式陳述式
+console.log(f1());
 function f1(){
     let str = 'this is funcion one';
     return str;
 }
-console.log(f1());
 
+//函式表達式 (提升性 hoisting)
 const f2 = function(){
     let str = 'this is function two';
     return str;
 }
-
 console.log(f2());
+
+
+//參數
+
+function f3(x){
+    return x*x;
+}
+
+console.log(f3(11));
+
+function f4(x,y){
+    return x * y;
+}
+
+console.log(f4(10,8));
+
+
 
